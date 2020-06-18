175b12a823ee8a77acb61de5d64ef6ab049a2e31
allgoodworks <allgoodworks79@gmail.com>
2020-06-18 15:51:51 +0300

    Fix ui issue

--- src/components/phonePanel/index.js
+++ src/components/phonePanel/index.js
@@ -10,25 +10,27 @@ library.add(far.faCaretSquareDown, far.faCaretSquareUp, far.faPlusSquare, far.fa
 
 const PhonePanel = props => {
   return (
-    <div className='story-right'>
-      <h3 className='story-right__title'>{props.title}</h3>
-      <div className='story-right__wrapper'>
-        <div className='story-right__img-nav-block'>
-          <img className='story-right__img' alt="phone" src={props.img} />
-          <div className='story-right__nav-wrapper'>
-            <FontAwesomeIcon icon={far.faPlusSquare} />
-            <FontAwesomeIcon icon={far.faCaretSquareUp} />
-            <FontAwesomeIcon icon={far.faCaretSquareDown} />
+    <div style={{height: '100%', display: 'flex', justifyContent: 'center'}} className='story-right'>
+      <div className='story-right'>
+        <h3 className='story-right__title'>{props.title}</h3>
+        <div className='story-right__wrapper'>
+          <div className='story-right__img-nav-block'>
+            <img className='story-right__img' alt="phone" src={props.img} />
+            <div className='story-right__nav-wrapper'>
+              <FontAwesomeIcon icon={far.faPlusSquare} />
+              <FontAwesomeIcon icon={far.faCaretSquareUp} />
+              <FontAwesomeIcon icon={far.faCaretSquareDown} />
+            </div>
           </div>
-        </div>
-        <div className='story-right__additional'>
-          <div className='story-right__additional-icons'>
-            <FontAwesomeIcon icon={far.faCopy} />
-            <i className="fa fa-trash-o" />
-          </div>
-          <div className='story-right__buttons-wrapper'>
-            <Button className='story-right__back' onClick={() => props.prevSteps(props.currentTab)} variant='secondary'>Back</Button>
-            <Button onClick={() => props.nextSteps(props.currentTab)} color='warning'>Next</Button>
+          <div className='story-right__additional'>
+            <div className='story-right__additional-icons'>
+              <FontAwesomeIcon icon={far.faCopy} />
+              <i className="fa fa-trash-o" />
+            </div>
+            <div className='story-right__buttons-wrapper'>
+              <Button className='story-right__back' onClick={() => props.prevSteps(props.currentTab)} variant='secondary'>Back</Button>
+              <Button onClick={() => props.nextSteps(props.currentTab)} color='warning'>Next</Button>
+            </div>
           </div>
         </div>
       </div>
